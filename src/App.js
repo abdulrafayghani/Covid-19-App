e92a1d4c4a1ebe07da4a974e6b1216adb5761346
abdulrafayghani <abdulrafay.ghani@gmail.com>
2020-07-31 21:54:35 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import { DataProvider } from './context/State';
 import { Header } from './components/header/Header';
 import { Background } from './components/Background/Background';
 import { Chart } from './components/chart/Chart';
+import { Loading } from './components/loading/Loading'
+
+// const BackGround = React.lazy( () => import('./components/Background/Background')) 
+// const Chart = React.lazy( () => import('./components/chart/Chart')) 
+
 
 const App = () => {
   return (
     <>
       <DataProvider>
-      <Suspense >
+      <Suspense fallback={ <Loading /> }>
           <Header /> 
           <Background />
          <Chart />
--- src/components/card/Cards.jsx
+++ src/components/card/Cards.jsx
@@ -1,5 +1,11 @@
 import React, { useContext, useEffect } from 'react';
-import { Card, CardContent, Typography, Box, makeStyles} from '@material-ui/core';
+import {
+  Card,
+  CardContent,
+  Typography,
+  Box,
+  makeStyles,
+} from '@material-ui/core';
 import { GlobalContext } from '../../context/State';
 import { fetchGlobalData } from '../../api/index';
 import CountUp from 'react-countup';
@@ -20,45 +26,105 @@ export const Cards = () => {
     updateData,
   } = useContext(GlobalContext);
 
-  useEffect(() => {
-    (async () => {
-      const globalData = await fetchGlobalData();
-      updateData(globalData);
-    })();
-  }, []);
+  useEffect(
+    () => {
+      (async () => {
+        const globalData = await fetchGlobalData();
+        updateData(globalData);
+      })();
+    }, // eslint-disable-next-line
+    []
+  );
 
   return (
-     <Box display="flex" flexDirection="column" alignItems="center">
-       <Box mb={1}>
-         <Card className={classes.root}>
-           <CardContent>
-             <Typography variant='inherit' style={{ fontWeight:'bold', display:'flex', justifyContent:'center', color:'#17a2b8'}} gutterBottom>CASES</Typography>
-             <Typography variant='h6' style={{display:'flex', justifyContent:'center', color:"#50658e", fontWeight:'bold'}}>
-               <CountUp art={0} end={confirmed} duration={2.5} separator=","/>
-             </Typography>
-           </CardContent>
-         </Card>
-       </Box>
-       <Box mb={1}>
-         <Card className={classes.root}>
-           <CardContent>
-             <Typography variant='inherit' style={{ fontWeight:'bold', display:'flex', justifyContent:'center', color:'#f86168'}} gutterBottom>FATALITIES</Typography>
-             <Typography variant='h6' style={{display:'flex', justifyContent:'center', color:"#50658e", fontWeight:'bold'}}>
-               <CountUp start={0} end={deaths} duration={2.5} separator=","/>
-             </Typography>
-           </CardContent>
-         </Card>
-       </Box>
-       <Box mb={1 }>
-         <Card className={classes.root}>
-           <CardContent>
-             <Typography variant='inherit' style={{ fontWeight:'bold', display:'flex', justifyContent:'center', color:'#0f9c00'}} gutterBottom>RECOVERED</Typography>
-             <Typography variant='h6' style={{display:'flex', justifyContent:'center', color:"#50658e", fontWeight:'bold'}}>
-               <CountUp start={0} end={recovered} duration={2.5} separator=","/>
-             </Typography>
-           </CardContent>
-         </Card>
-       </Box>
-     </Box>
+    <Box display="flex" flexDirection="column" alignItems="center">
+      <Box mb={1}>
+        <Card className={classes.root}>
+          <CardContent>
+            <Typography
+              variant="inherit"
+              style={{
+                fontWeight: 'bold',
+                display: 'flex',
+                justifyContent: 'center',
+                color: '#17a2b8',
+              }}
+              gutterBottom
+            >
+              CASES
+            </Typography>
+            <Typography
+              variant="h6"
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                color: '#50658e',
+                fontWeight: 'bold',
+              }}
+            >
+              <CountUp art={0} end={confirmed} duration={2.5} separator="," />
+            </Typography>
+          </CardContent>
+        </Card>
+      </Box>
+      <Box mb={1}>
+        <Card className={classes.root}>
+          <CardContent>
+            <Typography
+              variant="inherit"
+              style={{
+                fontWeight: 'bold',
+                display: 'flex',
+                justifyContent: 'center',
+                color: '#f86168',
+              }}
+              gutterBottom
+            >
+              FATALITIES
+            </Typography>
+            <Typography
+              variant="h6"
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                color: '#50658e',
+                fontWeight: 'bold',
+              }}
+            >
+              <CountUp start={0} end={deaths} duration={2.5} separator="," />
+            </Typography>
+          </CardContent>
+        </Card>
+      </Box>
+      <Box mb={1}>
+        <Card className={classes.root}>
+          <CardContent>
+            <Typography
+              variant="inherit"
+              style={{
+                fontWeight: 'bold',
+                display: 'flex',
+                justifyContent: 'center',
+                color: '#0f9c00',
+              }}
+              gutterBottom
+            >
+              RECOVERED
+            </Typography>
+            <Typography
+              variant="h6"
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                color: '#50658e',
+                fontWeight: 'bold',
+              }}
+            >
+              <CountUp start={0} end={recovered} duration={2.5} separator="," />
+            </Typography>
+          </CardContent>
+        </Card>
+      </Box>
+    </Box>
   );
 };
